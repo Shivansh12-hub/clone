21b452919d70ca2cb618bcf6b8a4cf7acd1a2aff
Shivansh Kumar <shivanshcoc11@gmail.com>
2026-07-31 15:26:41 +0500

    comment controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -2,6 +2,7 @@ import sharp from "sharp"
 import cloudinary from "../utils/cloudinary";
 import Post from "../models/post.model";
 import User from "../models/user.model";
+import Comment from "../models/comment.model";
 
 
 export const addNewPost = async (req, res) => {
@@ -61,12 +62,175 @@ export const addNewPost = async (req, res) => {
 
 export const getAllPost = async (req, res) => {
     try {
-        const post = (await Post.find()).toSorted({ createdAt: -1 })
+        const posts = await Post.find().toSorted({ createdAt: -1 })
             .populate({ path: 'author', select: 'userName, profilePicture' })
-        .populate({path:'comments', sort:{createdAt:-1}, })
+            .populate({
+                path: 'comments',
+                sort: { createdAt: -1 },
+                populate: ({
+                    path: 'author',
+                    select:'userName, profilePicture'
+                })
+            })
+        
+        return res.status(200).json({
+            success: true,
+            posts,
+            message:"Post successfully fetched",
+        });
         
     } catch (error) {
         console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+
+export const getUserPOst = async (req, res) => {
+    try {
+        const authorId = req.id;
+
+        const posts = await Post.find({ author: authorId })
+            .sort({ createdAt: -1 })
+            .populate({ path: 'author', select: 'userName, profilePicture' })
+            .populate({
+                path: 'comments',
+                sort: { createdAt: -1 },
+                populate: ({
+                    path: 'author',
+                    select:'userName, profilePicture'
+                })
+            })
+        
+        return res.status(200).json({
+            success: true,
+            posts,
+            message:"Post successfully fetched",
+        });
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+export const likePost = async (req, res) => {
+    try {
+        const likeKarneValaUser = req.id;
+        const postId = req.params.id;
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            return res.status(400).json({
+                success: false,
+                message:"post not found"
+            })
+        }
+
+        // like post logic
+        await post.updateOne({ $addToSet: { likes: likeKarneValaUser } });
+        await post.save();
+
+        // implement socket.io for real time notification
+
+
+
+        return res.status(200).json({
+            message: "Post Liked",
+            success:true
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+
+
+
+export const disLikePost = async (req, res) => {
+    try {
+        const disLikeKarneValaUser = req.id;
+        const postId = req.params.id;
+
+        const post = await Post.findById(postId);
+
+        if (!post) {
+            return res.status(400).json({
+                success: false,
+                message:"post not found"
+            })
+        }
+
+        // unlike post logic
+        await post.updateOne({ $pull: { likes: likeKarneValaUser } });
+        await post.save();
+
+        // implement socket.io for real time notification
+
+
+
+        return res.status(200).json({
+            message: "Post unLiked",
+            success:true
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+
+export const addComment = async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const commentKarneValekiId = req.id
+
+        const { text } = req.body;
+        if (!text) {
+            return res.status(400).json({
+                message: "Text is required",
+                success: false
+            });
+        }
+        const post = Post.findById(postId);
+
+        const comment = await Comment.create({
+            text,
+            author: commentKarneValekiId,
+            post: postId
+        })
+            .populate({ path: 'author', select: "userName, profilePicture" });
+        
+        post.comment.push(comment._id);
+        await post.save();
+
+        return res.status(200).json({
+            message: "Comment Added",
+            comment,
+            success:true
+        })
+
+        
+    } catch (error) {
+        console.log(error);
         return res.status(500).json({
             success: false,
             message: error.message,
--- backend/models/comment.model.js
+++ backend/models/comment.model.js
@@ -15,5 +15,6 @@ const commentSchema = new mongoose.Schema({
         required:true
     }
 })
-
-export default Comment = mongoose.model('Comment', commentSchema)
+const Comment = mongoose.model('Comment', commentSchema)
+export default Comment;
+    
